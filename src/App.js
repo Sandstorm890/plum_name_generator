83425baed90c0d6673a9e21b94994975dd4cee78
zaynegoering <zaynegoering@gmail.com>
2022-03-08 18:35:28 -0800

    add nameContainer to layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react'
 import NameShow from './components/NameShow.js';
+import NameContainer from './containers/NameContainer';
 
 class App extends React.Component {
   
   render() {
     return (
       <div className="App">
+        <NameContainer />
         <NameShow />
       </div>
     );
   }
-  
+
 }
 
 export default App;
